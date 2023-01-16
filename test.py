73ad37826be314c054fc5815abdbe2e2ae51d1d4
zcfabra <christianwgnam@gmail.com>
2023-01-16 10:21:15 -0500

    init

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import torch
+import numpy as np
+
+import os, sys
+
+
+def main():
+
+	args = sys.argv
+	if len(args) <2:
+		print("[X] MUST SPECIFCY AN IMAGE")
+		sys.exit(0)
+
+
+if __name__ == "__main__":
+	main()
+
